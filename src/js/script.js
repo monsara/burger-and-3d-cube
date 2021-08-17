2cb90d1743ea8df52d94eae9e79257226d532e51
Monsara <viktor.hirenko@gmail.com>
2021-08-18 02:15:22 +0300

    update JS for start video

--- src/js/script.js
+++ src/js/script.js
@@ -4,33 +4,62 @@ const cubeInit = () => {
   const cube = document.querySelector('.cube');
   const video = cube.querySelector('.media-box video');
   const videoOverlay = cube.querySelector('.media-box__overlay');
+  const unmuteBtn = cube.querySelector('.unmute-button');
 
+  const showVideoOverlay = () => videoOverlay.classList.remove('hide');
+  const hideVideoOverlay = () => videoOverlay.classList.add('hide');;
+  const showUnmuteBtn = () => (unmuteBtn.style.opacity = 1);
+  const hideUnmuteBtn = () => (unmuteBtn.style.opacity = 0);
+  
   cube.addEventListener('mouseenter', () => {
-      const playPromise = video.play();
+    if (!video) {
+      return;
+    }
 
-      if (playPromise !== undefined) {
-        playPromise
-        .then(_ => {})
-        .catch(error => {
-          console.log(error);
+    video.muted = true;
+    video.play();
 
-          videoOverlay.classList.remove('hide');
-        });
-      }
+    const isVideoLoaded = video.readyState === 4;
 
-      videoOverlay.classList.add('hide');
+    if (isVideoLoaded) {
+      hideVideoOverlay();
+      showUnmuteBtn();
+    }
 
-      console.log('Mouse enter');
+    // const playPromise = video.play();
+
+    // if (playPromise !== undefined) {
+    //   playPromise
+    //     .then(_ => {})
+    //     .catch(error => {
+    //       console.log(error);
+
+    //       showVideoOverlay();
+    //     });
+    // }
+
+    // hideVideoOverlay();
   });
 
   cube.addEventListener('mouseleave', () => {
     video.pause();
     video.currentTime = 0;
+    showVideoOverlay();
+    hideUnmuteBtn();
+  });
 
-    videoOverlay.classList.remove('hide');
+  unmuteBtn.addEventListener('click', (e) => {
+    const mediaBox = e.currentTarget.parentElement;
+    const isPressed = mediaBox.querySelector('.unmute-button--pressed');
 
-    console.log('Mouse out');
-    console.log('Pause');
+    if (!isPressed) {
+      video.muted = false;
+      unmuteBtn.classList.add('unmute-button--pressed');
+      return;
+    }
+    
+    unmuteBtn.classList.remove('unmute-button--pressed');
+    video.muted = true;
   });
 }
 
